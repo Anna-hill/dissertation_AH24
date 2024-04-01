11265ba856e72b8d88d1155a1291744766240e13
Anna Hill <s2559258@ed.ac.uk>
2024-04-01 13:51:55 +0100

    Adding elevation to plots

--- src/pandasTest.py
+++ src/pandasTest.py
@@ -2,35 +2,38 @@ from matplotlib import pyplot as plt
 import pandas as pd
 
 
+def metric_text(text_file, filename):
+
+    # read GEDI_metric text file output
+    df = pd.read_csv(text_file, delimiter=" ", header=0)
+
+    for index, row in df.iterrows():
+        true_height = row.iloc[1]
+
+        # height calculated by gedi processing
+        gedi_height = row.iloc[5]
+        print(true_height, "TH", gedi_height, "GH", index)
+
+    # plot csv of wave
+    data = pd.read_csv(filename)
+    data_filtered = data[data["wave"] > 0]
+    plt.plot(data_filtered["wave"], data_filtered["z"], color="green")
+    plt.axhline(gedi_height, color="red", label="GEDI ground")
+    plt.axhline(true_height, color="black", label="True ground")
+    plt.legend()
+    plt.ylabel("Elevation (m)")
+    plt.xlabel("DN")
+    # plt.title("Waveform with 0 wave values filtered out")
+    plt.savefig("Plot.321262.666327.png")
+    plt.clf()
+
+
 ######################################
 
 if __name__ == "__main__":
     """Main block"""
-    # Relative path to filename from OOSA-code-public folder
+
+    # read files
     filename = "data/plots.gediWave.321262.666327.csv"
-
-    # Read data in to RAM
-    data = pd.read_csv(filename)
-    print(data)
-    data_filtered = data[data["wave"] > 0]
-    print(data_filtered)
-
-    # Sort by the time column
-    # sortedData = data.sort_values("time").reset_index(drop=True)
-
-    # Print out the columns
-    # print(sortedData.columns)
-
-    # Mean of a column
-    # print("mean time", sortedData["time"].mean())
-
-    # Bounds
-    # print("x bounds", sortedData["lon"].min(), sortedData["lat"].max())
-    # print(min(data["wave"]))
-
-    plt.plot(data_filtered["wave"], data_filtered["z"], color="green")
-    # plt.ylim(200, None)
-    plt.ylabel("Elevation (m)")
-    plt.xlabel("DN")
-    plt.title("Waveform with 0 wave values filtered out")
-    plt.show()
+    text_file = "data/wave_metrics.txt"
+    metric_text(text_file, filename)
--- src/pandasTest2.py
+++ src/pandasTest2.py
@@ -1,6 +1,15 @@
 import pandas as pd
 
-df = pd.read_csv("data/teastMetric.metric.txt", delimiter=" ")
+if __name__ == "__main__":
 
-print(df["lon,"])
-# header is a bit messy, so difficult to just pull out lon/lat
+    df = pd.read_csv("data/teastMetric.metric.txt", delimiter=" ", header=0)
+
+    for index, row in df.iterrows():
+        true_height = row.iloc[1]
+
+        # height calculated by gedi processing
+        gedi_height = row.iloc[5]
+        print(true_height, "TH", gedi_height, "GH")
+
+    # print(true_height)
+    # header is a bit messy, so difficult to just pull out lon/lat
