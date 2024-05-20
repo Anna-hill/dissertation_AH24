ad6320d4a105b172e85f4fc41ed65e1d17519861
Anna Hill <s2559258@ed.ac.uk>
2024-05-20 16:25:47 +0100

    testing waveform plotting

--- README.md
+++ README.md
@@ -6,4 +6,4 @@ teastMetric is a text file from GEDIMetric, values set according to defeault com
 # Random forest inspiration repo?
 https://github.com/euanmitchell/dissertation/blob/main/randomForestLaSelva.py?fbclid=IwZXh0bgNhZW0CMTAAAR3IsicGlxO0u7OIWCq35MwXTXyCzb2jJgESaRaVXzEb2mNHExP1gpjeoUQ_aem_ATb_y6Izy0lzdKaQKvLxPQaDP53R-VWldfGJ_uvXlyBY8qP6x-h8_AA1GzBH3MA8dZev_HfVS-GaodsUtGLl6WGD
 
-testShell aims to perform automated processing to simulate wveforms
+testShell aims to perform automated processing to simulate waveforms
--- src/testShell.py
+++ src/testShell.py
@@ -1,5 +1,6 @@
 import os
 import time
+import h5py
 import subprocess
 import argparse
 import h5py
@@ -34,6 +35,7 @@ def extractBounds(folder):
     for idx, file in enumerate(file_list):
         bounds = lasBounds.lasMBR(file)
         print(f"working on file {idx + 1} of {len(file_list)}, bounds = {bounds}")
+        outname = f"data/{folder}/sim_waves/Sim_{bounds[0]}{bounds[1]}.h5"
 
         rat_files = subprocess.run(
             [
@@ -49,11 +51,28 @@ def extractBounds(folder):
                 "-gridStep",
                 "30",
                 "-output",
-                f"Sim_{file}.h5",
+                outname,
                 "-hdf",
             ]
         )
+
         print("The exit code was: %d" % rat_files.returncode)
+
+        """plot_rats = subprocess.run(
+            [
+                "python3",
+                "$GEDIRAT_ROOT/gediHandler.py",
+                "--input",
+                outname,
+                "--outRoot",
+                f"data/{folder}/sim_waves/new_plots",
+            ]
+        )
+
+        print("The exit code was: %d" % plot_rats.returncode)"""
+        # The following command works in linux, but likely to be slower than alternatives
+        # Currently looks wierd because i have text file from past gedi metric run and they don't match
+        # python3	$GEDIRAT_ROOT/gediHandler.py --input data/Bonaly/sim_waves/Sim_320000.0665000.0.h5 --outRoot data/Bonaly/shell_plots/
 
 
 if __name__ == "__main__":
@@ -65,6 +84,15 @@ if __name__ == "__main__":
     # find las file in arg-defined study area folder
     # find bounds of las file
     extractBounds(study_area)
+
+    # in python window in terminal:
+    # sanity check of hdf5 files (better way later)
+    #    >>> import h5py
+    #    >>> filename = "data/Bonaly/sim_waves/Sim_321000.0666000.0.h5"
+    #    >>> f = h5py.File(filename, "r")
+    #    >>> list(f)
+    #    >>> list(f["LAT0"])
+
     # looping on bounds (OOSA), i.e. find bounds of 1/4 of las file
     # Run GediRat to simulate waveforms in each quarter
     # example:
