8a5fff41b627c897d66619cd7059101c96f88751
Anna-hill <145706889+Anna-hill@users.noreply.github.com>
2024-07-25 19:38:18 +0100

    Delete canopyOnly.bat

--- canopyOnly.bat
+++
@@ -1,8 +0,0 @@
-G:\LAStools\bin\lascanopy.exe –i Z:\s2559258\dissertation_AH24\data\Bonaly\als_height\*.las –odir Z:\s2559258\dissertation_AH24\data\Bonaly\als_canopy –otif –step 30 –dns –cover_cutoff 1.67
-G:\LAStools\bin\lascanopy.exe –i Z:\s2559258\dissertation_AH24\data\hubbard_brook\als_height\*.las –odir Z:\s2559258\dissertation_AH24\data\hubbard_brook\als_canopy –otif –step 30 –dns –cover_cutoff 1.67
-G:\LAStools\bin\lascanopy.exe –i Z:\s2559258\dissertation_AH24\data\la_selva\als_height\*.las –odir Z:\s2559258\dissertation_AH24\data\la_selva\als_canopy –otif –step 30 –dns –cover_cutoff 1.67
-G:\LAStools\bin\lascanopy.exe –i Z:\s2559258\dissertation_AH24\data\nouragues\als_height\*.las –odir Z:\s2559258\dissertation_AH24\data\nouragues\als_canopy –otif –step 30 –dns –cover_cutoff 1.67
-G:\LAStools\bin\lascanopy.exe –i Z:\s2559258\dissertation_AH24\data\oak_ridge\als_height\*.las –odir Z:\s2559258\dissertation_AH24\data\oak_ridge\als_canopy –otif –step 30 –dns –cover_cutoff 1.67
-G:\LAStools\bin\lascanopy.exe –i Z:\s2559258\dissertation_AH24\data\paracou\als_height\*.las –odir Z:\s2559258\dissertation_AH24\data\paracou\als_canopy –otif –step 30 –dns –cover_cutoff 1.67
-G:\LAStools\bin\lascanopy.exe –i Z:\s2559258\dissertation_AH24\data\robson_creek\als_height\*.las –odir Z:\s2559258\dissertation_AH24\data\robson_creek\als_canopy –otif –step 30 –dns –cover_cutoff 1.67
-G:\LAStools\bin\lascanopy.exe –i Z:\s2559258\dissertation_AH24\data\wind_river\als_height\*.las –odir Z:\s2559258\dissertation_AH24\data\wind_river\als_canopy –otif –step 30 –dns –cover_cutoff 1.67
