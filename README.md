c307de4018351211634abc0330638f4725b7b966
Anna Hill <s2559258@ed.ac.uk>
2024-07-29 18:55:25 +0100

    added plotting options

--- README.md
+++ README.md
@@ -53,3 +53,6 @@ python3 src/shellSquared.py --studyarea all --lassettings all
 
 python3 src/analyseResults.py --studyarea la_selva --lassettings 400505 --interpolation _linear
 python3 src/analyseResults.py --studyarea hubbard_brook --lassettings 400505 --interpolation _linear
+
+
+python3 src/slope_cc_plot.py --studyarea all
--- src/slope_cc_plot.py
+++ src/slope_cc_plot.py
@@ -4,7 +4,8 @@ import numpy as np
 from matplotlib import pyplot as plt
 from glob import glob
 import pandas as pd
-from scipy import stats
+from scipy import stats, optimize
+from matplotlib.animation import FuncAnimation
 from lasBounds import append_results
 from plotting import folder_colour
 from analyseResults import analysisCommands
@@ -12,13 +13,14 @@ from analyseResults import analysisCommands
 
 def find_merged(folder):
     file_path = f"data/{folder}/merged_rasters"
-    # data/bonaly/merged_rasters
+    # data/Bonaly/merged_rasters/b_p149_n15_diff_linear.tif
 
-    canopy_list = glob(file_path + f"/canopy.tif")
-    slope_list = glob(file_path + f"/slope.tif")
-    diff_list = glob(file_path + f"/diff.tif")
-    # print(canopy_list, slope_list, diff_list)
-    return canopy_list[0], slope_list[0], diff_list[0]
+    canopy_list = glob(file_path + f"/*canopy.tif")
+    slope_list = glob(file_path + f"/*slope.tif")
+    diff_l_list = glob(file_path + f"/*diff_linear.tif")
+    diff_c_list = glob(file_path + f"/*diff_cubic.tif")
+    print(canopy_list, slope_list, diff_l_list, diff_c_list)
+    return canopy_list[0], slope_list[0], diff_l_list[0], diff_c_list[0]
 
 
 def open_raster(tif):
@@ -38,24 +40,34 @@ def pad_array(arr, target_shape):
 def slope_cc(folder):
     """Open tif files and return flat arrays"""
 
-    canopy_tif, slope_tif, diff_tif = find_merged(folder)
+    canopy_tif, slope_tif, diff_l_tif, diff_c_tif = find_merged(folder)
 
     read_canopy = open_raster(canopy_tif)
     read_slope = open_raster(slope_tif)
-    read_diff = open_raster(diff_tif)
+    read_l_diff = open_raster(diff_l_tif)
+    read_c_diff = open_raster(diff_c_tif)
 
     # find max array dims
     max_shape = (
-        max(read_canopy.shape[0], read_slope.shape[0], read_diff.shape[0]),
-        max(read_canopy.shape[1], read_slope.shape[1], read_diff.shape[1]),
+        max(
+            read_canopy.shape[0],
+            read_slope.shape[0],
+            read_l_diff.shape[0],
+            read_c_diff.shape[0],
+        ),
+        max(
+            read_canopy.shape[1],
+            read_slope.shape[1],
+            read_l_diff.shape[1],
+            read_c_diff.shape[1],
+        ),
     )
 
     # Resize arrays to the maximum shape
     canopy_padded = pad_array(read_canopy, max_shape)
     slope_padded = pad_array(read_slope, max_shape)
-    diff_padded = pad_array(read_diff, max_shape)
-
-    print(canopy_padded.shape, slope_padded.shape, diff_padded.shape)
+    diff_l_padded = pad_array(read_l_diff, max_shape)
+    diff_c_padded = pad_array(read_c_diff, max_shape)
 
     # remove 0 values and ensure same raster dims
     """combined_mask = (canopy_padded > 0) and (slope_padded > 0) and (diff_padded != 0)
@@ -68,19 +80,37 @@ def slope_cc(folder):
     # flatten arrays for stats
     flat_canopy = canopy_padded.flatten()
     flat_slope = slope_padded.flatten()
-    flat_diff = diff_padded.flatten()
+    flat_l_diff = diff_l_padded.flatten()
+    flat_c_diff = diff_c_padded.flatten()
 
     # convert all negative vals to pos for abs error
-    flat_diff = np.where(flat_diff < 0, -flat_diff, flat_diff)
+    # flat_diff = np.where(flat_diff < 0, -flat_diff, flat_diff)
 
-    return flat_canopy, flat_slope, flat_diff
+    # append arrays into df
+    """merged_rasters = {
+        "canopy": [],
+        "slope": [],
+        "linear_difference": [],
+        "cubic_difference": [],
+    }
+    append_results(
+        merged_rasters,
+        canopy=flat_canopy,
+        slope=flat_slope,
+        linear_difference=flat_l_diff,
+        cubic_difference=flat_c_diff,
+    )
+    merged_rasters_df = pd.DataFrame(merged_rasters)"""
+    all_arrays = np.vstack([flat_canopy, flat_slope, flat_l_diff, flat_c_diff])
+
+    return all_arrays
 
 
-def plot_matrix(sites, type):
+def plot_matrix(sites, plot_data):
 
     plt.rcParams["font.family"] = "Times New Roman"
     plt.rcParams["figure.constrained_layout.use"] = True
-    plt.rcParams["figure.figsize"] = (8, 10)
+    plt.rcParams["figure.figsize"] = (8, 12)
     plt.rcParams["xtick.labelsize"] = 10
     plt.rcParams["xtick.major.size"] = 2
     plt.rcParams["xtick.major.width"] = 0.4
@@ -99,6 +129,7 @@ def plot_matrix(sites, type):
         "P_value": [],
         "intercept": [],
         "std_err": [],
+        "slope": [],
     }
 
     subplot_len = int(len(sites) / 2)
@@ -110,59 +141,207 @@ def plot_matrix(sites, type):
     axes = axes.flatten()
 
     for site, ax in zip(sites, axes):
-        canopy, slope, diff = slope_cc(site)
+        all_arrays = slope_cc(site)
+        print(f"raster values for {site}")
+        print(all_arrays[0])
 
-        # convert diff values to absolute?
-        # try log transform on CC
-        # Perform linear regression
+        # If type == 1
+        if plot_data == 1:
+            # return df error (linear) as var y, and slope as var x
+            var_x = all_arrays[1]
+            var_y = abs(all_arrays[2])
+
+            # filter out 0 values
+            non_zero_mask = (
+                (var_x != 0) & (var_y != 0) & (var_y <= 50)
+            )  # Oak ridge has an outlier pixel at 100m error
+
+            # only slope values within given cc range
+            range_mask = (all_arrays[0] >= 0.9) & (all_arrays[0] <= 0.95)
+
+            # Combine the masks
+            combined_mask = non_zero_mask & range_mask
+
+            # Apply the mask to filter the arrays
+            var_x = var_x[combined_mask]
+            var_y = var_y[combined_mask]
+
+            axis_x = "Slope (°)"
+            axis_y = "Absolute elevation error (m)"
+            outname = "slope"
+
+        elif plot_data == 2:
+            # return df error (linear) as var y, and CC as var x
+            var_x = all_arrays[0]
+            var_y = abs(all_arrays[2])
+
+            # filter out 0 values
+            non_zero_mask = (
+                (var_x != 0) & (var_y != 0) & (var_y <= 50)
+            )  # Oak ridge has an outlier pixel at 100m error
+
+            # only cc values within given slope range
+            range_mask = (all_arrays[1] >= 10) & (all_arrays[1] <= 15)
+
+            # Combine the masks
+            combined_mask = non_zero_mask & range_mask
+
+            # Apply the mask to filter the arrays
+            var_x = var_x[combined_mask]
+            var_x = var_x * 100
+            var_y = var_y[combined_mask]
+
+            axis_x = "Canopy Cover (%)"
+            axis_y = "Absolute elevation error (m)"
+            outname = "canopy"
+
+        # If type == 3
+        # return df error (linear) as var y, and error (cubic) as var x
+        # set axis labels
+        # set title
+        # set output file names
+
+        else:
+            # return df error (linear) as var y, and error (cubic) as var x
+            var_x = all_arrays[3]
+            var_y = all_arrays[2]
+
+            # filter out 0 values
+            non_zero_mask = (
+                (var_x != 0) & (var_y != 0) & (abs(var_y) <= 50) & (abs(var_x) <= 50)
+            )  # Oak ridge has one outlier pixel at 100m error
+
+            # only cc values within given slope range
+            # range_mask = (all_arrays[1] >= 10) & (all_arrays[1] <= 15)
+
+            # Apply the mask to filter the arrays
+            # if site == "oak_ridge":
+            var_y = abs(var_y[non_zero_mask])
+            var_x = abs(var_x[non_zero_mask])
+
+            lims = (min(min(var_x), min(var_y)), max(max(var_x), max(var_y)))
+            # y_lims = (min(var_y), max(var_y))
+
+            axis_x = "cubic elevation error (m)"
+            axis_y = "Linear elevation error (m)"
+            outname = "interpolation"
+
+        # Descriptive statistics?
+        # print(stats.describe(var_x))
+        # print(stats.describe(var_y))
+
         slope_linreg, intercept, r_value, p_value, std_err = stats.linregress(
-            canopy, diff
+            var_x,
+            var_y,
         )
         append_results(
             results,
             Folder=site,
-            R2=r_value,
+            R2=(r_value**2),
             P_value=p_value,
             intercept=intercept,
             std_err=std_err,
+            slope=slope_linreg,
         )
-
-        # Calculate the regression line
-        line = slope_linreg * canopy + intercept
 
         # Plot the data
-        ax.scatter(canopy, diff, label="Data", alpha=0.4, color=folder_colour(site))
-        ax.plot(
-            canopy,
-            line,
-            color="red",
-            label=f"Linear fit: y={slope_linreg:.2f}x + {intercept:.2f}",
-        )
+        ax.scatter(var_x, var_y, alpha=0.2, color=folder_colour(site))
 
         # Add title and labels
         ax.set_title(f"{site} (R²={r_value**2:.2f}, p={p_value:.2e})")
-        ax.set_xlabel("Canopy Cover")
-        ax.set_ylabel("Elevation error (m)")
-        ax.set_ylim(-40, 40)  #
-        ax.set_xlim(0, 1)  #
-        # ax.legend()
+        ax.set_xlabel(axis_x)
+        ax.set_ylabel(axis_y)
+        if plot_data == 3:
+            # ensure diff interpolation models have same x y lims
+            ax.set_ylim(lims)  #
+            ax.set_xlim(lims)  #
 
     # Adjust layout and show plot
     plt.tight_layout()
-    plt.savefig(f"figures/canopy_regress.png")
+    plt.savefig(f"figures/{outname}_regress.png")
     plt.clf()
+
+    ##########
 
     # save results to new csv
     resultsDf = pd.DataFrame(results)
-    outCsv = f"data/canopy_regress.csv"
+    outCsv = f"data/{outname}_regress.csv"
     resultsDf.to_csv(outCsv, index=False)
     print("Results written to: ", outCsv)
+
+    ###########
+
+    """def plot3D(df):
+
+        # set plot settings
+        plt.rcParams["font.family"] = "Times New Roman"
+        plt.rcParams["figure.constrained_layout.use"] = True
+        plt.rcParams["figure.figsize"] = (8, 6)
+        plt.rcParams["legend.fontsize"] = 10
+        plt.rcParams["legend.frameon"] = False
+        plt.rcParams["xtick.labelsize"] = 10
+        plt.rcParams["xtick.major.size"] = 2
+        plt.rcParams["xtick.major.width"] = 0.4
+        plt.rcParams["xtick.major.pad"] = 2
+        plt.rcParams["ytick.labelsize"] = 10
+        plt.rcParams["ytick.major.size"] = 2
+        plt.rcParams["ytick.major.width"] = 0.4
+        plt.rcParams["ytick.major.pad"] = 2
+        plt.rcParams["axes.labelsize"] = 12
+        plt.rcParams["axes.linewidth"] = 0.5
+        plt.rcParams["axes.labelpad"] = 3
+        plt.rcParams["axes.titlesize"] = 14
+        plt.rcParams["lines.linewidth"] = 1
+        plt.rcParams["lines.markersize"] = 4
+
+        fig = plt.figure()
+        ax = fig.add_subplot(projection="3d")
+
+        for site in df["Folder"].unique():
+            site_group = df[df["Folder"] == site]
+            X = site_group["nPhotons"]
+            Y = site_group["Noise"]
+            Z = site_group["beam_sensitivity"]
+            scat = ax.scatter(X, Y, Z, color=folder_colour(site), label=site)
+        ax.set_xlabel("Photons")
+        ax.set_ylabel("Noise")
+        ax.set_zlabel("Beam Sensitivity")
+        ax.legend(loc="upper left")
+
+        plt.title(f"Beam Sensitivity for {las_settings} las settings")
+
+        # create static image
+        fig.savefig(f"figures/scatter_plots/{las_settings}.png")
+
+        # Function to update the view angle
+        def update(frame):
+            ax.view_init(elev=30, azim=frame)
+            return (scat,)
+
+        # Create an animation
+        ani = FuncAnimation(
+            fig, update, frames=np.arange(0, 360, 1), interval=50, blit=True
+        )
+
+        # Save the animation as a GIF
+        ani.save(
+            f"figures/scatter_plots/rotating_{las_settings}.gif", writer="imagemagick"
+        )
+        print(f"Gif saved to f 'figures/scatter_plots/rotating_{las_settings}.gif'")
+
+        # close figure
+        fig.close()"""
 
 
 if __name__ == "__main__":
     t = time.perf_counter()
     cmdargs = analysisCommands()
     site = cmdargs.studyArea
+    plot_type = cmdargs.plotType
+    # types:
+    # 1 = Slope
+    # 2 = Canopy
+    # 3 = Interpolation method
 
     if site == "all":
         study_sites = [
@@ -170,16 +349,16 @@ if __name__ == "__main__":
             "hubbard_brook",
             "la_selva",
             "nouragues",
-            # "oak_ridge",
+            "oak_ridge",
             "paracou",
             "robson_creek",
-            # "wind_river",
+            "wind_river",
         ]
         print(f"working on all sites ({study_sites})")
-        plot_matrix(study_sites)
+        plot_matrix(study_sites, plot_type)
 
     else:
-        plot_matrix([site])
+        plot_matrix([site], plot_type)
 
     t = time.perf_counter() - t
     print("time taken: ", t, " seconds")
